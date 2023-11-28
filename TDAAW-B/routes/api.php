e00243817d1b43c2113fe2661a27bbb6116303c0
Diego Salazar Jara <diego.salazar2101@alumnos.ubiobio.cl>
2023-11-28 00:07:17 -0300

    Creacion rutas

--- TDAAW-B/routes/api.php
+++ TDAAW-B/routes/api.php
@@ -2,18 +2,12 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PerrosController;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
-
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
+Route::group(['prefix' => 'perro'], function () {
+    Route::post('newPerro', [PerrosController::class, 'registrarPerro']);
+    Route::put('updatePerro/{id}', [PerrosController::class, 'actualizarPerro']);
+    Route::get('getAllPerros', [PerrosController::class, 'listarPerros']);
+    Route::get('getPerro/{id}', [PerrosController::class, 'listarPerro']);
+    Route::delete('deletePerro/{id}', [PerrosController::class, 'eliminarPerro']);
 });
