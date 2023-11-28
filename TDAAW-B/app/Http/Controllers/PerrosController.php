f7407984de2a57a0956f43ed6aed47b04ef16036
Diego Salazar Jara <diego.salazar2101@alumnos.ubiobio.cl>
2023-11-28 00:08:26 -0300

    Implementacion PerrosController

---
+++ TDAAW-B/app/Http/Controllers/PerrosController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\{ListarPerroRequest, PerroRequest, PerroUpdateRequest};
+use Illuminate\Http\Request;
+use App\Repositories\PerroRepository;
+
+class PerrosController extends Controller
+{
+    protected PerroRepository $perroRepository;
+
+    public function __construct(PerroRepository $perroRepository)
+    {
+        $this->perroRepository = $perroRepository;
+    }
+
+    public function registrarPerro(PerroRequest $request)
+    {
+        return $this->perroRepository->registrarPerro($request);
+    }
+
+    public function actualizarPerro($id, PerroUpdateRequest $request)
+{
+    return $this->perroRepository->actualizarPerro($id, $request);
+}
+
+    public function listarPerros(Request $request)
+    {
+        return $this->perroRepository->listarPerros($request);
+    }
+
+    public function listarPerro($id, Request $request)
+    {
+        return $this->perroRepository->listarPerro($id, $request);
+    }
+
+    public function eliminarPerro($id)
+    {
+        return $this->perroRepository->eliminarPerro($id);
+    }
+}
